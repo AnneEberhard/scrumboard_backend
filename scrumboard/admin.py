075a73a1547cb8697544262fffcfc91904d703d2
AnneEberhard <anne.eberhard@gmx.net>
2024-02-11 13:02:38 +0100

    added models and first admin view

--- scrumboard/admin.py
+++ scrumboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CustomUser, UserDefCategory, Task
 
-# Register your models here.
+class userAdmin(admin.ModelAdmin):
+    list_display = ('username','first_name',  'last_name')
+    search_fields = ('acronym',)
+
+class taskAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'author','title',  'column_category')
+    search_fields = ('title', 'priority')
+
+admin.site.register(CustomUser, userAdmin)
+admin.site.register(Task, taskAdmin)
+admin.site.register(UserDefCategory)
--- scrumboard/models.py
+++ scrumboard/models.py
@@ -1,3 +1,26 @@
+from datetime import date
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=15)
+    acronym = models.CharField(max_length=3)
+    color = models.CharField(max_length=15)
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
+
+
+class UserDefCategory(models.Model):
+    title = models.CharField(max_length=15)
+    color = models.CharField(max_length=15)
+
+
+class Task(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateField(default=date.today)
+    title = models.CharField(max_length=15)
+    description = models.TextField()
+    due_date = models.DateField()
+    priority = models.CharField(max_length=15)
+    column_category = models.CharField(max_length=15)
+    subtasks = models.TextField(blank=True, default='[]')
