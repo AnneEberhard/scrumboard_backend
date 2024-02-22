0587b4dcd0c7d63ce06b38a1f33f646059ec9940
AnneEberhard <anne.eberhard@gmx.net>
2024-02-22 09:22:03 +0100

    requirements update

--- scrumboard/models.py
+++ scrumboard/models.py
@@ -2,7 +2,7 @@ from datetime import date
 from django import apps
 from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission, BaseUserManager
+from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.hashers import make_password
 
 
