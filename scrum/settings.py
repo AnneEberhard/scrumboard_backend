78a9becb5be1e699dd6650822d47922b798dc49b
AnneEberhard <anne.eberhard@gmx.net>
2024-02-27 10:01:14 +0100

    added cors allowed origins

--- scrum/settings.py
+++ scrum/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['localhost',
     '127.0.0.1',
     'anne.pythonanywhere.com']
 
-CORS_ALLOWED_ORIGINS = [ '*', 'localhost', '127.0.0.1', "http://localhost:5500", "http://127.0.0.1:5500"]
+CORS_ALLOWED_ORIGINS = [ '*', 'localhost', '127.0.0.1', "http://localhost:5500", "http://127.0.0.1:5500","https://anne-eberhard.developerakademie.net/scrum-frontend"]
 
 CORS_ALLOW_CREDENTIALS = True 
 
