1331cabe261bc022cfd5b286f8a30375966f6cd7
AnneEberhard <anne.eberhard@gmx.net>
2024-02-27 13:36:05 +0100

    updated email

--- scrum/settings.py
+++ scrum/settings.py
@@ -152,9 +152,11 @@ REST_FRAMEWORK = {
 }
 from decouple import config
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.mail.pythonanywhere.com'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_HOST = 'smtp.mail.pythonanywhere.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
