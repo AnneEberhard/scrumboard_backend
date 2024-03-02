bce1b409a826b3a5206be3a0a4712adfb205e373
AnneEberhard <anne.eberhard@gmx.net>
2024-03-02 09:08:58 +0100

    expanded title length

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import date
-from django.conf import settings
-
+from django.conf import settings#
 
 from contacts.models import Contact
 
@@ -11,7 +10,7 @@ This models defines the tasks
 class Task(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     created_at = models.DateField(default=date.today)
-    title = models.CharField(max_length=15, default='')
+    title = models.CharField(max_length=30, default='')
     description = models.TextField(default='')
     category = models.CharField(max_length=30, default='')
     assignedContacts = models.ManyToManyField(Contact)
